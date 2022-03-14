60d62c2aff37f5af611e879b6b2934e289f6d3f9
Liam Pereira <pereira.l.h@hotmail.ca>
2022-03-14 19:52:42 -0400

    Fixed code

--- main.py
+++ main.py
@@ -3,20 +3,44 @@ from scheduler import Scheduler
 import time
 start_time = time.time()
 
-process_list = []
+with open('input.txt', 'r') as file:
+    lines = file.readlines()
 
-for i in range(Process.NumberOfProcesses):
-    p = Process(Process.NumberOfProcesses,Process.process_id[i],Process.arrivalTime[i],Process.burstTime[i],Process.priority[i])
+    # get the time quantum of the program
+number_of_processes = int(lines.pop(0))
+
+# dictionary containing the user names and number of processes under each user
+process_id = []
+# list containing the ready time for each process
+arrival_time = []
+# list containing the service time for each process
+burst_time = []
+# list containing the priority of each process
+priority = []
+
+for line in lines:
+
+    process_id.append((line.split(" ")[0]))
+    arrival_time.append((line.split(" ")[1]))
+    burst_time.append(int(line.split(" ")[2]))
+    priority.append(int(line.split(" ")[3]))
+
+file.close()
+
+
+process_list = []
+for i in range(number_of_processes):
+    p = Process(number_of_processes,process_id[i],arrival_time[i],burst_time[i],priority[i])
     process_list.append(p)
-for i in range(Process.NumberOfProcesses):
+for i in range(number_of_processes):
     print('--------------------------------')
     process_list[i].print_info()
 
 
-s = Scheduler(process_list)
+#s = Scheduler(process_list)
 
-s.initialize_scheduler()
-s.print_q2_info()
+#s.initialize_scheduler()
+#s.print_q2_info()
 
 
 
--- process.py
+++ process.py
@@ -2,36 +2,9 @@
 
 class Process:
 
-    # open input.txt and read file content
-    with open('input.txt', 'r') as file:
-        lines = file.readlines()
-
-    # get the Number of Processes of the program
-    NumberOfProcesses = int(lines.pop(0))
-
-    # list containing the process id for each process
-    process_id = []
-    # list containing the Arrival Time for each process
-    arrivalTime = []
-    # list containing the Burst time for each process
-    burstTime = []
-    # list containing the Priority for each process
-    priority = []
-
-    # store the user _name and number of processes as well as each process' ready time and service time in their respective variables
-    for line in lines:
-        process_id.append(str(line.split()[0]))
-        arrivalTime.append(int(str(line.split()[1])))
-        burstTime.append(int(str(line.split()[2])))
-        priority.append(int(str(line.split()[3])))
-
-    # close the file
-    file.close()
-
-
-
     #Constructor
     def __init__(self,total_processes,pid,arrival_time,burst_time,priority):
+        self.number_of_processes = total_processes
         self.pid = pid
         self.arrival_time = arrival_time
         self.burst_time = burst_time
@@ -75,8 +48,3 @@ class Process:
         print("process arrival time is: " + str(self.get_arrival_time()))
         print("process burst time is: " + str(self.get_burst_time()))
         print("process priority is: " +  str(self.get_priority()))
-
-
-
-
-
